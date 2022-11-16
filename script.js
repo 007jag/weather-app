4aa60d77d871d93772c0a6a9970fb8e7c2de87be
007jag <114781974+007jag@users.noreply.github.com>
2022-11-16 15:55:00 -0500

    added search and set up API, now can grab city and data from search box

--- script.js
+++ script.js
@@ -9,3 +9,24 @@ var currentHumidty= $("#humidity");
 var currentWSpeed=$("#wind-speed");
 var currentUvindex= $("#uv-index");
 var sCity=[];
+
+//searches the city to see if it exists in the entries from the storage
+function find(c){
+    for (var i=0; i<sCity.length; i++){
+        if(c.toUpperCase()===sCity[i]){
+            return -1;
+        }
+    }
+    return 1;
+}
+
+//Set up the API key and display the curent and future weather to the user after grabing the city form the input text box.
+var APIKey="a0aca8a89948154a4182dcecc780b513";
+
+function displayWeather(event){
+    event.preventDefault();
+    if(searchCity.val().trim()!==""){
+        city=searchCity.val().trim();
+        currentWeather(city);
+    }
+}
